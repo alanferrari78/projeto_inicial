e52975b0d5f73b1bbb4cf1dd9c6db85a40416fd9
Alan Ferrari <alanfolha@gmail.com>
2024-07-28 21:40:20 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Jornada de dados")
+
+# Variaveis
+idade = 34
+nome = "Luciano"
+
+#Print
+print(idade)
+
+#soma
+nova_idade = idade + 1
+print(nova_idade)
+
+#Listas
+listas = [1,2,3,4,5]
+
+# Métodos
+lista_invertida = listas.reverse()
+print(lista_invertida)
